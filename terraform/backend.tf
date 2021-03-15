d9e02b9b7827f45a39dbbc97a0e6514420aef05f
dapperandy <andeeje@icloud.com>
2021-03-15 12:45:55 +0000

    Add files via upload

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+#Set S3 backend for persisting TF state
+terraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
+  backend "s3" {
+    region  = "eu-west-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "spaceytf"
+  }
+}
